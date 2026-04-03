0d7008df9843ee11eb0681ba053cee93e8e7e7ec
solodevss <chukwunnoso.isu@gmail.com>
2026-04-04 00:07:41 +0100

    Update metadata for Venarium branding

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,8 @@
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>Please sign in to your account.</p>
+    </div>
+  );
+}
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <p>Create a new account to start tracking your job applications.</p>
+    </div>
+  );
+}
