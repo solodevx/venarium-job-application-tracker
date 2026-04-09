e2082c7373c07fc5a105270905cb47a2f72b465a
solodevss <chukwunnoso.isu@gmail.com>
2026-04-09 18:37:10 +0100

    setup auth flow, updated styling sign in & signout helper done with auth - navbar updated

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </p>
             <div className="flex flex-col items-center gap-4">
               <Link href="/sign-up">
-                <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                <Button size="lg" className="h-12 px-8 text-lg font-medium ">
                   Start for free <ArrowRight className="ml-2" />
                 </Button>
               </Link>
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,8 +1,11 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { MongoClient } from "mongodb";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import connectDB from "../db";
 
-const client = new MongoClient(process.env.MONGODB_URI!);
+const mongooseInstance = await connectDB();
+const client = mongooseInstance.connection.getClient();
 const db = client.db();
 
 export const auth = betterAuth({
@@ -13,3 +16,21 @@ export const auth = betterAuth({
     enabled: true,
   },
 });
+
+export async function getSession() {
+  const result = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return result;
+}
+
+export async function signOut() {
+  const result = await auth.api.signOut({
+    headers: await headers(),
+  });
+
+  if (result.success) {
+    redirect("/sign-in");
+  }
+}
